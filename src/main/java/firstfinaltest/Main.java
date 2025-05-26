bccd4a2e2a0c5e6296bc42569087d570c18815ec
szonavy <viktoriaszotaknagy@gmail.com>
2025-05-26 19:40:43 -0400

    Practice the task from amazon ( first modul)

---
+++ src/main/java/firstfinaltest/Main.java
@@ -0,0 +1,27 @@
+package firstfinaltest;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Sandy";
+        String lastName = "Jones";
+        String movieTitle = "The Source Code";
+        int numberOfTicket = 8;
+        double price = 10.57;
+        double cost = numberOfTicket * price;
+
+        String userName = firstName+lastName;
+        userName = userName.toLowerCase();
+
+        String convertTitle = movieTitle.toUpperCase();
+
+        System.out.println("Congratulations!! You have successfully booked the tickets\n");
+        System.out.println("Username: " + userName);
+        System.out.println("Movie: " + convertTitle);
+        System.out.println("Number of tickets: " + numberOfTicket);
+        System.out.println("Price per ticket: $" + price);
+        System.out.println("Total price of tickets: $" + cost);
+        System.out.println("\nThank you for choosing us for booking your movie tickets");
+        System.out.println("Enjoy your movie!!!");
+
+    }
+}
---
+++ src/main/java/firsttask/MyFirstProgram.java
@@ -0,0 +1,8 @@
+package firsttask;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Let’s change the code now…");
+        System.out.println("it’s what I do!");
+    }
+}
